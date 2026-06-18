ef46f99604470d7921520c4338f2bedd583d47cb
Sandhiya6363 <selvarajsandhiya86@gmail.com>
2026-06-18 07:48:47 +0000

    second commit

---
+++ menu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("<-----------------WELCOME TO THE AROMA RESTAURANT----------------->");
+    printf("List of Items");
+    printf("1.STARTER\n 2.MAINCOURSE\n 3.DESSERT\n 4.BEVERAGES\n 5.EXIT");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case '1':
+        printf("")
+    }
+}
